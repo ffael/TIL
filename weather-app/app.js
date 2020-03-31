9cd956b3521a3595a5a1d92d940f4ead40242ea0
Rafael <ffael@live.com>
2020-03-30 22:52:23 -0400

    Changed location and added summary to the message

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,14 @@
 const axios = require('axios')
 
-const url = `https://api.darksky.net/forecast/${process.env.APIKEY}/37.8267,-122.4233`
+const url = `https://api.darksky.net/forecast/${process.env.APIKEY}/40.7487727,-73.9849336`
 
 async function getData(){
   try{
-    const res = await axios.get(url)
+    const res = await axios.get(url, { params: { units: 'us', exclude:'flags, minutely, hourly'} })
     const { temperature, precipProbability } = res.data.currently
-    console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
+    const { daily } = res.data
+    
+    console.log(`${daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
   }catch(err){
     console.log(err)
   }
