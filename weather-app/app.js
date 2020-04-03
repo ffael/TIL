b1b9fa7a7c0686f421cd5dcbc9b552d1d36cf3d2
Rafael <ffael@live.com>
2020-04-03 11:43:53 -0400

    Refactored app.js to take location from terminal

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,15 @@
 const getGeoCoding = require('./utils/geocode')
 const getForecast = require('./utils/forecast')
 
-getGeoCoding('Boston', (place, lon, lat)=>{
-  getForecast(lon, lat, 'us', 'en', (temperature, precipProb, daily)=>{
-    console.log(place)
-    console.log(`${daily.summary} It's currently ${Math.round(temperature)} degrees.`)
+const address = process.argv[2]
+
+if(!address){
+  console.log('Please provide a valid address!')
+}else{
+  getGeoCoding(address, (place, lon, lat)=>{
+    getForecast(lon, lat, 'us', 'en', (temperature, daily)=>{
+      console.log(place)
+      console.log(`${daily.summary} It's currently ${Math.round(temperature)} degrees.`)
+    })
   })
-})
+}
