e6fd0e7819b79504e5ac782f2a1568d88b5dab51
Rafael <ffael@live.com>
2020-04-02 23:32:55 -0400

    Refactored to work with callback chain

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,9 @@
+const getGeoCoding = require('./utils/geocode')
 const getForecast = require('./utils/forecast')
 
-getForecast('Boston', 'us', 'en')
+getGeoCoding('Boston', (place, lon, lat)=>{
+  getForecast(lon, lat, 'us', 'en', (temperature, precipProb, daily)=>{
+    console.log(place)
+    console.log(`${daily.summary} It's currently ${Math.round(temperature)} degrees.`)
+  })
+})
