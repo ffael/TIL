a00045ed358df1246b2091f3c388a3ddd796f2bf
Rafael <ffael@live.com>
2020-04-03 11:41:38 -0400

    Deleted not used data

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,10 +4,10 @@ async function getForecast(lon, lat, units='us', lang="en", callBack){
   const weatherURL = `https://api.darksky.net/forecast/${process.env.APIKEY}/`
   try{
     const res = await axios.get(`${weatherURL}${lon},${lat}`, { params: { units, exclude:'flags, minutely, hourly', lang } })
-    const { temperature, precipProb } = res.data.currently
+    const { temperature } = res.data.currently
     const { daily } = res.data
 
-    callBack(temperature, precipProb, daily)
+    callBack(temperature, daily)
   }catch(err){
     console.log('Error with weather service!')
   }
