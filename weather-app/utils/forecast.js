afadcb34838d9903a76c14c252874b00588b555d
Rafael <ffael@live.com>
2020-04-02 23:32:25 -0400

    Refactored to expect callback as parameter

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,21 +1,16 @@
 const axios = require('axios')
-const getGeoCoding = require('./geocode')
 
-async function getForecast(location, units='us', lang="en"){
+async function getForecast(lon, lat, units='us', lang="en", callBack){
   const weatherURL = `https://api.darksky.net/forecast/${process.env.APIKEY}/`
-  getGeoCoding(location , async (place, lat, lon)=>{
-    try{
-      const res = await axios.get(`${weatherURL}${lon},${lat}`, { params: { units, exclude:'flags, minutely, hourly', lang } })
-  
-      const { temperature, precipProbability } = res.data.currently
-      const { daily } = res.data
-      
-      console.log(place)
-      console.log(`${daily.data[0].summary} It is currently ${Math.round(temperature)}degrees out. There is a ${precipProbability}% chance of rain.`)
-    }catch(err){
-      console.log('Error with weather service!')
-    }
-  })
+  try{
+    const res = await axios.get(`${weatherURL}${lon},${lat}`, { params: { units, exclude:'flags, minutely, hourly', lang } })
+    const { temperature, precipProb } = res.data.currently
+    const { daily } = res.data
+
+    callBack(temperature, precipProb, daily)
+  }catch(err){
+    console.log('Error with weather service!')
+  }
 }
 
 module.exports = getForecast
