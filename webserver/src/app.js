f8d652b4c6c57c342108dd607bd40082098262ee
Rafael <ffael@live.com>
2020-04-05 12:13:59 -0400

    Added publicDir path

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,9 +1,17 @@
+const path = require('path')
 const express = require('express')
+
+const publicDir = path.join(__dirname, '../public/')
 
 const app = express()
 
-app.get('/', (req, res)=>{
-  res.send('<h1>Hello Express!</h1>')
+app.use(express.static(publicDir))
+
+app.get('/weather', (req, res)=>{
+  res.send({
+    forecast: 'Cloudy',
+    location: 'Boston, MA'
+  })
 })
 
 app.listen(3000, ()=>{
